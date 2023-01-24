27100583c1f84051b73e45fff4cf3b48d42a3af6
rohith goli <rohith.goli@anywhere.co>
2023-01-24 17:00:50 +0530

    action payload

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/iceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <IceCreamContainer />
         <HooksCakeContainer />
         <CakeContainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   );
