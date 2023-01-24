bb54d919af204078962d7cc3b5d3f8b50d7ae451
rohith goli <rohith.goli@anywhere.co>
2023-01-24 10:12:10 +0530

    logger middleware for redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger' // default exports are imported like this
 import rootReducer from './rootReducer'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 export default store
